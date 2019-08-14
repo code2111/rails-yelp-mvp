c7427de775e9029eee45d901caeb60384ee95045
Christophe Bensmaine <christophe.bensmaine@laposte.net>
2019-08-14 12:51:47 +0200

    Rails new with frontend and form gems

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,10 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+
+
+
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 10 fake restaurants...'
+Restaurant.destroy_all
+10.times do
+  restaurant = Restaurant.new(
+    name:    Faker::Company.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    phone_number: ["0","1","2","3","4","5","6","7","8","9"].sample(10).join
+  )
+  restaurant.save!
+end
+puts 'Finished!'
